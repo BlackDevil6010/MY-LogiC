3764df0792bd8b9e38357f3422db63eafe80ae63
Yugesh <yugeshdhanasekaran@gmail.com>
2026-03-08 01:48:07 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     UPLOAD_FOLDER = "uploads"
