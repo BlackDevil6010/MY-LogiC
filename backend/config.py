e6a37957de67b7c3deed6ea19e3fabc3ff363e3c
Yugesh <yugeshdhanasekaran@gmail.com>
2026-03-08 01:53:23 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,12 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise RuntimeError("DATABASE_URL is not set!")
+
+    SQLALCHEMY_DATABASE_URI = database_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
