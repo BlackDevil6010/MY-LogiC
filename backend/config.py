7e48b1fc1659d15c1918ca1a95971c5dc9b1782d
Yugesh <yugeshdhanasekaran@gmail.com>
2026-03-08 14:48:00 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,10 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-    if not SQLALCHEMY_DATABASE_URI:
-        raise RuntimeError("❌ DATABASE_URL is not set in environment")
-
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     UPLOAD_FOLDER = "uploads"
