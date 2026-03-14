698e507e426c9d5252e663eb9d1042492e8559e9
Yugesh <yugeshdhanasekaran@gmail.com>
2026-03-14 16:57:45 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -16,3 +16,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = "uploads"
+        # Email Settings
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in ['true', '1', 't']
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'yugeshdhanasekaran@gmail.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'xrhv wtca rvxk xxib')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', MAIL_USERNAME)
